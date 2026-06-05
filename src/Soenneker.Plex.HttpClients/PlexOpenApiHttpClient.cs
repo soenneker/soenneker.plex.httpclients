335bdd82708b1c902fa73d21f7d9e5e75d5b6e8a
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:12 -0500

    xml commenting

--- src/Soenneker.Plex.HttpClients/PlexOpenApiHttpClient.cs
+++ src/Soenneker.Plex.HttpClients/PlexOpenApiHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class PlexOpenApiHttpClient : IPlexOpenApiHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(nameof(PlexOpenApiHttpClient));
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(nameof(PlexOpenApiHttpClient));
